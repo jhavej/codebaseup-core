f05aa0171fede7fc4835db95fefb89a752771c5d
Jakub Havej <jakub.havej@gmail.com>
2023-10-06 18:10:07 +0200

    chore: obsolete code cleaning vol.2

--- apps/docs/app/layout.tsx
+++
@@ -1,7 +0,0 @@
-const RootLayout = ({ children }: React.PropsWithChildren) => (
-  <html lang="en">
-    <body>{children}</body>
-  </html>
-);
-
-export default RootLayout;
--- apps/web/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare interface Window {
-  dataLayer?: Array<unknown>; // Google Analytics GA4
-}
