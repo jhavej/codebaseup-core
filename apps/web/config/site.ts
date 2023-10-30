9c38de6eca9ea5bbe77a3f5f786fbd819761f097
Jakub Havej <jakub.havej@gmail.com>
2023-10-30 07:53:43 +0100

    chore: mktng copy next.js version

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "CodebaseUp - Core",
   description:
-    "The Next.js (App Router) boilerplate that saves you endless hours of setup hassle.",
+    "The Next.js (v14+ App Router) boilerplate that saves you endless hours of setup hassle.",
   url: "https://www.codebaseup.com",
 };
